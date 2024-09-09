b6c66ad7967752b36a5876a9939d32be90ad5ed9
alex cesar <thealexcesar@gmail.com>
2024-09-09 18:17:12 -0300

    📦 feature: structure

---
+++ src/main/java/com/authclinic/AuthClinicApplication.java
@@ -0,0 +1,13 @@
+package com.authclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthClinicApplication.class, args);
+    }
+
+}
